043c7c925428f19e809d6c95a38d7cb693b4107c
Samiksha Dnyandev Tonde <stonde2005@gmail.com>
2026-07-15 18:47:10 +0530

    Add files via upload

---
+++ Java-Programming/Practice/LB Class/program66.java
@@ -0,0 +1,66 @@
+//QUESTION = check wheter the number is Prime or not
+import java.util.Scanner;
+class NumberX
+{
+
+    public boolean CheckPrime(int iNo)
+    {
+        int iCnt = 0;
+       
+
+        for (iCnt=2 ; iCnt<=(iNo/2) ; iCnt++)               //2 pasun start karan 1 prime ahe always so lavkar baher padel
+        {
+            if(iNo%iCnt==0)
+            {
+                break;
+            }
+            if(iCnt>=iNo/2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+        
+    }
+    
+
+}
+class program66
+{
+   
+    public static void main(String A[])                         //main body
+    {
+
+        Scanner sobj = new Scanner (System.in);                 //object creation                
+
+        int iValue = 0;
+        boolean bRet = false;                                    //rekami pishvi
+
+    
+
+
+        System.out.println("Enter no");
+        iValue=sobj.nextInt();
+        
+        NumberX nobj = new NumberX();
+        bRet = nobj.CheckPrime(iValue);                                    //call
+
+
+        if(bRet==true)
+        {
+            System.out.println("It is Prime");
+        }
+        else
+        {
+            System.out.println("It is not Prime");
+            
+        }
+ 
+    }
+}
+//Time Complexity : O(N/2)
+//Where N>=0
---
+++ Java-Programming/Practice/LB Class/program68.java
@@ -0,0 +1,63 @@
+//QUESTION = check wheter the number is Prime or not
+import java.util.Scanner;
+class NumberX
+{
+
+    public boolean CheckPrime(int iNo)
+    {
+        int iCnt = 0;
+        boolean bFlag = true ;
+       
+
+        for (iCnt=2 ; iCnt<=(iNo/2) ; iCnt++)               //2 pasun start karan 1 prime ahe always so lavkar baher padel
+        {
+            if((iNo%iCnt)==0)
+            {
+               bFlag=false;
+               break;
+            }
+
+        }
+
+        return bFlag;
+    
+        
+    }
+    
+
+}
+class program68
+{
+   
+    public static void main(String A[])                         //main body
+    {
+
+        Scanner sobj = new Scanner (System.in);                 //object creation                
+
+        int iValue = 0;
+        boolean bRet = false;                                    //rekami pishvi
+
+    
+
+
+        System.out.println("Enter no");
+        iValue=sobj.nextInt();
+        
+        NumberX nobj = new NumberX();
+        bRet = nobj.CheckPrime(iValue);                                    //call
+
+
+        if(bRet==true)
+        {
+            System.out.println("It is Prime");
+        }
+        else
+        {
+            System.out.println("It is not Prime");
+            
+        }
+ 
+    }
+}
+//Time Complexity : O(N/2)
+//Where N>=0
