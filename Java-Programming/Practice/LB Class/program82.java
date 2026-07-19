0155745a065cf49ac1ee68193b7ac381ae1ced87
Samiksha Dnyandev Tonde <stonde2005@gmail.com>
2026-07-19 22:40:24 +0530

    Add files via upload

---
+++ Java-Programming/Practice/LB Class/program82.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class DigitX
+{
+    public void DisplayDigits(int iNo)
+    {
+        int iDigit=0;
+        for(;iNo!=0;)
+        {
+            iDigit=iNo%10;
+            System.out.println(iDigit);
+            iNo=iNo/10;
+
+        }
+    }
+}
+class program81
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new 
+    }
+
+}
